81289fe2c5f778bc12a4f3ca226cba2292423d76
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-07-19 22:00:22 +0700

    Try auto deploy

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! Deploy nich...</p>"
 
 @app.route("/works")
 def it_works():
-    return "IT Works!"
+    return "IT Works! nyehehe"
